6b0d5d1b8d29d8683e0caf7899e410e0cf33b8de
Paulalemany <palemany@ucm.es>
2024-02-15 10:17:31 +0100

    ruta de archivo y lee archivo

--- Práctica 1/Program.cs
+++ Práctica 1/Program.cs
@@ -7,8 +7,10 @@ namespace Práctica_1
 
         const bool DEBUG = true;
 
+        #region Structs
         struct Coor {   // coordenadas en el tablero  
-            public int x, y; }
+            public int x, y;
+        }
 
         struct Pilar {  // pilar en el tablero 
             public Coor coor;   // posición en el tablero
@@ -22,12 +24,23 @@ namespace Práctica_1
             public Pilar[] pils;    // array de pilares
             public Rect[] rects;    // array de rectángulos
             public int numRects; }  // num de rectángulos definidos = prim pos libre en rect
+        #endregion
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Tablero tab = new Tablero();    //Creamos el tablero
+            string file;
+
+            Inicializa(out file);           //Creamos la ruta de acceso del tablero
+
+            LeeNivel(file, tab);
+
         }
 
+        #region Métodos
+
+        #region Input
         static char leeInput()
         {
             char d = ' ';
@@ -62,8 +75,26 @@ namespace Práctica_1
             }
             return d;
         }
+        #endregion
 
-        void LeeNivel(string file, Tablero tab)
+        #region inicialización
+
+        static void Inicializa(out string file)
+        {
+            //Incicializamos el nivel
+            Console.WriteLine("Escriba el número del nivel que desea jugar ");      //Tal vez sea mejor sacar esta línea y dejar el método mudo
+            string nivel = Console.ReadLine();
+            char[] cadena = nivel.ToCharArray();
+
+            if (cadena.Length < 3)
+            {
+                for (int i = cadena.Length; i <= 2; i++) { nivel = "0" + nivel; }
+            }
+
+            file = "puzzles/puzzles/" + nivel + ".txt";
+        }
+
+        static void LeeNivel(string file, Tablero tab)
         {
             //Abrimos el archivo que se va a leer
             StreamReader sr = new StreamReader(file);
@@ -71,6 +102,42 @@ namespace Práctica_1
             //Creamos el tablero
             tab.fils = int.Parse(sr.ReadLine());        //Filas del tablero
             tab.cols = int.Parse(sr.ReadLine());        //Columnas del tablero
+
+            tab.pils = new Pilar[tab.cols * tab.fils];  //inicialización del array -> EN EL ENUNCIADO PONE QUE DEBE TENER UNA COMPONENTE POR PILAR <-
+            tab.rects = new Rect[tab.cols * tab.fils];  //Inicialización array de rectángulos
+
+            int j = 0;      //Contador de filas
+            int k = 0;      //Contador de pilares
+            while (!sr.EndOfStream)
+            {
+                //Leemos la fila del tablero
+                string fila = sr.ReadLine();
+                //Dividimos la fila por espacios
+                string[] pilares = fila.Split(' ');
+                //Guardamos los números ignorando los guiones
+                for (int i = 0; i < pilares.Length; i++)
+                {
+                    if (pilares[i] == "-") { }  //Si es un guion lo ignoramos
+                    else
+                    {
+                        //Creamos un pilar
+                        Pilar pil = new Pilar();
+                        pil.val = int.Parse(pilares[i]);
+                        pil.coor.x = i;
+                        pil.coor.y = j;
+
+                        //Guardamos el pilar en el array
+                        tab.pils[k] = pil;
+                        k++;
+                    }
+                }
+                j++;
+            }
+
         }
+        #endregion
+
+        #endregion
+
     }
 }
